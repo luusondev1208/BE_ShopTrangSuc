ac9d42fcc6e377eca8049074e097437f709cdcb1
LuuSohn <luuhoangson2002@gmail.com>
2023-09-19 12:45:25 +0700

    [BE] Check permissions and AccessToken

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/category')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 
 
-router.post('/', ctrls.createdCategory)
+router.post('/', [verifyAccessToken, isAdmin],ctrls.createdCategory)
 router.get('/',  ctrls.getAllCategory)
-router.put('/:pcid', ctrls.updateCategory)
-router.delete('/:pcid',  ctrls.deleteCategory)
+router.put('/:pcid',[verifyAccessToken, isAdmin], ctrls.updateCategory)
+router.delete('/:pcid', [verifyAccessToken, isAdmin],ctrls.deleteCategory)
 
 
 
