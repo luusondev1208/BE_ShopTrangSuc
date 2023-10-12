30dc1af435d425899aaf64bae5cd45edafcd1732
LuuSohn <luuhoangson2002@gmail.com>
2023-10-13 03:44:25 +0700

    [BE] Upload Image Blog

--- config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2
-          
-cloudinary.config({ 
-  cloud_name: 'dz4cezwre', 
-  api_key: '161353539585881', 
-  api_secret: 'lHNv_eu1hzDa9l5088k1g2bFh60' 
-});
-
-module.exports = cloudinary
--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/blog')
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
+const upload = require('../config/cloudinary.config')
 
 
 router.post('/', [verifyAccessToken, isAdmin], ctrls.createdBlog)
@@ -8,6 +9,7 @@ router.put('/update/:bid', [verifyAccessToken, isAdmin], ctrls.updateBlog)
 router.get('/',  ctrls.getBlogs)
 router.get('/one/:bid',  ctrls.getBlog)
 router.delete('/:bid', [verifyAccessToken, isAdmin], ctrls.deleteBlog)
+router.put('/image/:bid', [verifyAccessToken,isAdmin],upload.single('image'), ctrls.uploadImageBlog)
 
 
 
