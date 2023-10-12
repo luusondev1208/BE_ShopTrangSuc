9ef66a25109129ad52ade2f018f86c76a566e03c
LuuSohn <luuhoangson2002@gmail.com>
2023-10-12 16:09:30 +0700

    [BE] CRUD User

--- controllers/user.js
+++ controllers/user.js
@@ -55,10 +55,75 @@ const login = asyncHandler(async (req, res) => {
     }
 })
 
+// GET ra 1 người dùng
+const getCurrent = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: user ? true : false,
+        rs: user ? user : 'Ko tìm thấy người dùng'
+    })
+})
+
+
+// Hiển thị tài khoản người dùng
+const getUsers = asyncHandler(async (req, res) => {
+    const response = await User.find().select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        users: response
+    })
+})
+
+
+
+// Xóa tài khoản User
+const deleteUser = asyncHandler(async (req, res) => {
+    const { _id } = req.query
+    if (!_id) throw new Error('Missing inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedUser: response ? `Người dùng ${response.email} đã bị xóa` : 'Tài khoản ko tồn tại'
+    })
+})
+
+
+// Cập nhập Người dùng
+const updateUser = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    if (!_id || Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Some thing went wrong'
+    })
+})
+
+
+// Cập nhập Admin
+const updateUserByAdmin = asyncHandler(async (req, res) => {
+    const { uid } = req.params
+    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    const response = await User.findByIdAndUpdate(uid, req.body, { new: true }).select('-password -role -refreshToken')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Some thing went wrong'
+    })
+})
+
 
 module.exports = {
     register,
     login,
+    getCurrent,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin,
+
+
+
    
     
 }
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,16 @@ const router = require('express').Router()
 const ctrls = require('../controllers/user')
 
 
+const {verifyAccessToken,isAdmin} = require('../middlewares/verifyToken')
 
 router.post('/register', ctrls.register)
 router.post('/login', ctrls.login)
+router.get('/current', verifyAccessToken, ctrls.getCurrent)
 
+router.get('/', [verifyAccessToken, isAdmin], ctrls.getUsers)
+router.delete('/', [verifyAccessToken, isAdmin], ctrls.deleteUser)
+router.put('/current', [verifyAccessToken], ctrls.updateUser)
+router.put('/:uid', [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
 
 
 
