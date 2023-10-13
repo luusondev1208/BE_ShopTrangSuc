3622328bf803c3c20360a06445326c007b1bfbab
LuuSohn <luuhoangson2002@gmail.com>
2023-10-13 19:18:52 +0700

    fix

--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const upload = require('../controllers/upload')
-
-router.post('/', upload.fields([{name: "img", maxCount: 1}]), (req, res) =>{
-    const link_img = req.files['img'][0];
-    res.send(link_img)
-})
-module.exports = router
