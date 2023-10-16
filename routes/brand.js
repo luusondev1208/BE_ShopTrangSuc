279c638d5ee6b83968d391b6a2df5d6328a54d2b
LuuSohn <luuhoangson2002@gmail.com>
2023-10-17 03:43:30 +0700

    [BE] CRUD Brand

---
+++ routes/brand.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const ctrls = require('../controllers/brand')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
+
+
+
+router.post('/', [verifyAccessToken, isAdmin], ctrls.createdBrand)
+router.get('/',  ctrls.getAllBrand)
+router.put('/:brid', [verifyAccessToken, isAdmin], ctrls.updateBrand)
+router.delete('/:brid', [verifyAccessToken, isAdmin], ctrls.deleteBrand)
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const dbConnect = require('./config/dbconnect')
 const initRoutes = require('./routes')
 const cookieParser = require('cookie-parser')
+const cors = require('cors'); 
 
 
 
@@ -13,6 +14,7 @@ const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 dbConnect()
