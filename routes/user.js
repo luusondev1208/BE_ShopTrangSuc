3810939275a93526ac1e72445b1a09f525b2e73c
LuuSohn <luuhoangson2002@gmail.com>
2023-11-06 13:21:27 +0700

    [BE] fix rest password

--- routes/user.js
+++ routes/user.js
@@ -7,18 +7,16 @@ const {verifyAccessToken,isAdmin} = require('../middlewares/verifyToken')
 router.post('/register', ctrls.register)
 router.post('/login', ctrls.login)
 router.get('/current', verifyAccessToken, ctrls.getCurrent)
-
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
+router.get('/forgotpassword', ctrls.forgotPassword)
+router.put('/resetpassword', ctrls.resetPassword)
 router.get('/', [verifyAccessToken, isAdmin], ctrls.getUsers)
 router.delete('/', [verifyAccessToken, isAdmin], ctrls.deleteUser)
 router.put('/current', [verifyAccessToken], ctrls.updateUser)
 router.put('/address', [verifyAccessToken], ctrls.updateUserAddress)
 router.put('/cart', [verifyAccessToken], ctrls.updateCart)
-router.post('/refreshtoken', ctrls.refreshAccessToken)
-
 router.put('/:uid', [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
-router.get('/logout', ctrls.logout)
-router.get('/forgotpassword', ctrls.forgotPassword)
-router.put('/resetpassword', ctrls.resetPassword)
 
 
 
