9be5cbf1ef155052da26f538ace4f04c77b7ae1f
LuuSohn <luuhoangson2002@gmail.com>
2023-11-10 22:30:53 +0700

    [BE] Blog Category

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const orderRouter = require('./order')
 const productRouter = require('./product')
 const blogRouter = require('./blog')
 const brandRouter = require('./brand')
+const blogCategoryRouter = require('./blogCategory')
 
 
 const initRoutes = (app) => {
@@ -14,6 +15,7 @@ const initRoutes = (app) => {
     app.use('/api/category',categoryRouter),
     app.use('/api/product',productRouter)
     app.use('/api/blog',blogRouter)
+    app.use('/api/blogcategory',blogCategoryRouter)
     app.use('/api/brand',brandRouter)
     app.use('/api/order',orderRouter)
 
