87aa8e4fb02f66122b0d884c9dea08a823579c87
LuuSohn <luuhoangson2002@gmail.com>
2023-11-17 21:36:57 +0700

    [BE] fix role user

--- controllers/user.js
+++ controllers/user.js
@@ -29,34 +29,37 @@ const register = asyncHandler(async (req, res) => {
 // Access token => Xác thực người dùng, quân quyên người dùng
 // Đăng Nhập
 const login = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password)
+    const { email, password } = req.body;
+    if (!email || !password) {
         return res.status(400).json({
-            sucess: false,
-            mes: 'Ko được bỏ trống'
-        })
-    // plain object
-    const response = await User.findOne({ email })
-    if (response && await response.isCorrectPassword(password)) {
-        // Tách password và role ra khỏi response
-        const { password, refreshToken, ...userData } = response.toObject()
-        // Tạo access token
-        const accessToken = generateAccessToken(response._id)
-        // Tạo refresh token
-        const newRefreshToken = generateRefreshToken(response._id)
-        // Lưu refresh token vào database
-        await User.findByIdAndUpdate(response._id, { refreshToken: newRefreshToken }, { new: true })
-        // Lưu refresh token vào cookie
-        res.cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
-        return res.status(200).json({
-            sucess: true,
-            accessToken,
-            userData
-        })
-    } else {
-        throw new Error('Thông tin ko hợp lệ!')
+            success: false,
+            mes: 'Ko được bỏ trống',
+        });
     }
-})
+
+    const response = await User.findOne({ email });
+
+    if (response && (await response.isCorrectPassword(password))) {
+        
+        const { password: _, ...userData } = response.toObject();
+
+        const accessToken = generateAccessToken(response._id, response.role);
+        const newRefreshToken = generateRefreshToken(response._id);
+
+        await User.findByIdAndUpdate(response._id, { refreshToken: newRefreshToken }, { new: true });
+
+        res.cookie('refreshToken', newRefreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+
+        return res.status(200).json({
+            success: true,
+            accessToken,
+            userData,
+        });
+    } else {
+        throw new Error('Thông tin không hợp lệ!');
+    }
+});
+
 // GET ra 1 người dùng
 // const getCurrent = asyncHandler(async (req, res) => {
 //     const { _id } = req.params
